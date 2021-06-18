b091a219e6d9d591052d356e71f72ca3033026e1
Ana Solomonia <ana.solomonia@gmail.com>
2021-06-18 17:13:17 +0400

    2 განდომილებიანის მინიჭება, ბეჭდვა, ჯამი.

--- twoDimentional/Program.cs
+++ twoDimentional/Program.cs
@@ -6,7 +6,41 @@ namespace twoDimentional
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] array = new int [3, 4];
+
+            // მინიჭება
+            Random r = new Random();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    array[i,j] = r.Next(10);
+                }
+            }
+
+            // ჯამი
+            int sum = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    sum += array[i,j];
+                }
+            }
+            Console.WriteLine($"Sum is {sum}");
+
+            // ბეჭდვა
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(array[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
