082b43c115730d1d30c62be33ec2b55e766c6247
Ana Solomonia <ana.solomonia@gmail.com>
2021-06-18 11:44:35 +0400

    new project

---
+++ twoDimentional/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace twoDimentional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
