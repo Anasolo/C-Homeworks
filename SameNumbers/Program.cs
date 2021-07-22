8f08f5b6483eae0d4737397d6a779ae9866a7f70
Ana Solomonia <ana.solomonia@gmail.com>
2021-07-22 16:10:59 +0400

    new task

---
+++ SameNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SameNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int[] numbers = new int[10];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = r.Next(10);
+                Console.WriteLine(numbers[i]);
+                if (i != 0 && numbers[i] == numbers[i - 1])
+                {
+                    Console.WriteLine("same");
+                }
+            }
+           
+        }
+    }
+}
