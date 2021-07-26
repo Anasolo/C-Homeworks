419bcd2ec4cc7e676ee19ba4c955ff9acf36a550
Ana Solomonia <ana.solomonia@gmail.com>
2021-07-26 16:36:43 +0400

    Sort and Reverse

---
+++ Reverse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int[] array = new int[5];
+
+            Console.WriteLine("Before reverse...");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(100);
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine("\n\nAfter reverse...");
+            for (int i = 0; i < array.Length/2; i++)
+            {
+                int temp = 0;
+                temp = array[i];
+                array[i] = array[(array.Length-1)-i];
+                array[(array.Length - 1) - i] = temp;
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int[] array = new int[10];
+            Console.WriteLine("Before sorting...");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(100);
+                Console.WriteLine(array[i]);
+            }
+
+            Console.WriteLine("\nAfter sorting...");
+
+            int temp = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
